f4c9520e10c11195b22c51fbfd0fa5537cfc53b9
SilasHakuzwimana <hakusilas@gmail.com>
2026-04-28 15:48:04 +0200

    Fix views for locaations

--- location/views.py
+++ location/views.py
@@ -1,42 +1,55 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .models import Province, District, Sector, Cell, Village
+from .models import Country, Province, District, Sector, Cell, Village  # Add Country
 from .serializers import *
 from django.contrib.auth.models import User
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters  # Add this for search
 
 # Create your views here.
 
 class CountryViewSet(viewsets.ModelViewSet):
     queryset = Country.objects.all()
     serializer_class = CountrySerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    search_fields = ['name']
     
     
 class ProvinceViewSet(viewsets.ModelViewSet):
     queryset = Province.objects.all()
     serializer_class = ProvinceSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['country']
+    search_fields = ['name']
 
 
 class DistrictViewSet(viewsets.ModelViewSet):
     queryset = District.objects.all()
     serializer_class = DistrictSerializer
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['name']
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['province', 'name']  # Add 'province' here
+    search_fields = ['name']
+
 
 class SectorViewSet(viewsets.ModelViewSet):
     queryset = Sector.objects.all()
     serializer_class = SectorSerializer
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['name', 'district']
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['district', 'name']
+    search_fields = ['name']
+
 
 class CellViewSet(viewsets.ModelViewSet):
     queryset = Cell.objects.all()
     serializer_class = CellSerializer
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['name', 'sector']
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['sector', 'name']
+    search_fields = ['name']
+
 
 class VillageViewSet(viewsets.ModelViewSet):
     queryset = Village.objects.all()
     serializer_class = VillageSerializer
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['name', 'cell']
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['cell', 'name']
+    search_fields = ['name']

d3dfd0b074a7c77231c8db635e67a9cf7a7e7cdd
SilasHakuzwimana <hakusilas@gmail.com>
2026-04-28 14:58:51 +0200

    fix: Fix the host settings for deployment

--- rwanda_admin_api/settings.py
+++ rwanda_admin_api/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-d*(zv3ybisw^um6ima*)7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,rwanda-administrative-structure-api.onrender.com').split(',')
 
 # Application definition
 INSTALLED_APPS = [
